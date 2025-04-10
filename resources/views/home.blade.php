bb873e314f055b004c51db504cea06f17301c159
unknown <fabrosjhordan.bsit@gmail.com>
2025-04-10 09:30:07 +0800

    little change on navbar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,7 +13,7 @@
             <div>
                 <img class="w-20 cursor-pointer" src="{{ URL ('images/solsticelogo.png')}}" alt="...">
             </div>
-            <div class="nav-links duration-500 md:static absolute bg-white md:min-h-fit min-h-[60vh] left-0 top-[-100%] md:w-auto w-full flex items-center px-5">
+            <div class="nav-links duration-500 md:static absolute bg-white md:min-h-fit min-h-[40vh] left-0 top-[-100%] md:w-auto w-full flex items-center px-5">
                 <ul class="flex md:flex-row flex-col md:items-center md:gap-[4vw] gap-8">
                     <li>
                         <a class="hover:text-gray-500"  href="#">Home</a>
