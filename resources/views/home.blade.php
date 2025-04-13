a6a4678c680adb9ba2cfef8672b21739fdc06a6d
unknown <fabrosjhordan.bsit@gmail.com>
2025-04-14 07:47:00 +0800

    UI: Search Bar and Item Cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -32,12 +32,19 @@
 
 </head>
 <body>
-   <header class="bg-white shadow-md top-0 left-0 w-full">
+   <header class="bg-white shadow-md fixed z-50 top-0 left-0 w-full">
         <nav class="flex justify-between items-center w-[92%] mx-auto">
             <div>
-                <img class="w-20 cursor-pointer" src="{{ URL ('images/solsticelogo.png')}}" alt="...">
+            <a href="/  "><img class="w-14 cursor-pointer" src="{{ URL ('images/solsticelogo.png')}}" alt="..."></a>
             </div>
-            <div class="nav-links z-[999] duration-500 md:static absolute bg-white md:min-h-fit min-h-[40vh] left-0 top-[-100%] md:w-auto w-full flex items-center px-5">
+            
+            <div class="flex items-center gap-6">
+                <p><a href="/auth" class="rounded-full bg-black text-white px-3 py-2 hover:bg-gray-400 cursor-pointer">Sign In</a></p>
+                <ion-icon onclick="onToggleMenu(this)" name="menu-outline" class="text-2xl cursor-pointer md:hidden"></ion-icon>
+            </div>
+        </nav>
+    </header>
+    <div class="nav-links z-[999] duration-500 md:static absolute bg-white md:min-h-fit min-h-[40vh] left-0 top-[-100%] md:w-auto w-full flex items-center px-5">
                 <ul class="flex md:flex-row flex-col md:items-center md:gap-[4vw] gap-8">
                     <li>
                         <a class="hover:text-gray-500"  href="#">Home</a>
@@ -53,15 +60,9 @@
                     </li>
                 </ul>
             </div>
-            <div class="flex items-center gap-6">
-                <p><a href="/auth" class="rounded-full bg-black text-white px-5 py-2 hover:bg-gray-400 cursor-pointer">Sign In</a></p>
-                <ion-icon onclick="onToggleMenu(this)" name="menu-outline" class="text-3xl cursor-pointer md:hidden"></ion-icon>
-            </div>
-        </nav>
-    </header>
 <div class="justify-items-center">
      <!--HTML CODE-->
-    <div class="w-full relative mt-10">
+    <div class="w-full relative pt-20">
     <div class="swiper max-w-[94%] centered-slide-carousel swiper-container relative ">
     <div class="swiper-wrapper">
     <div class="swiper-slide">
@@ -101,6 +102,76 @@
     
     <!-- Button Shop -->
      <button type="submit" class="rounded-sm border py-2 px-4 m-3 cursor-pointer hover:bg-gray-300">Shop Now!</button>
+
+    <!-- Search Bar -->
+    <form class="w-full max-w-[92%] my-5 mb-20">
+        <div class="flex">
+            <label for="search-dropdown" class="mb-2 text-sm font-medium text-gray-900 sr-only">Your Email</label>
+            <button id="dropdown-button" data-dropdown-toggle="dropdown" class="shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-s-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100" type="button">All categories <svg class="w-2.5 h-2.5 ms-2.5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
+        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
+    </svg></button>
+            <div id="dropdown" class="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44">
+                <ul class="py-2 text-sm text-gray-700" aria-labelledby="dropdown-button">
+                <li>
+                    <button type="button" class="inline-flex w-full px-4 py-2 hover:bg-gray-100">Mockups</button>
+                </li>
+                <li>
+                    <button type="button" class="inline-flex w-full px-4 py-2 hover:bg-gray-100">Templates</button>
+                </li>
+                <li>
+                    <button type="button" class="inline-flex w-full px-4 py-2 hover:bg-gray-100">Design</button>
+                </li>
+                <li>
+                    <button type="button" class="inline-flex w-full px-4 py-2 hover:bg-gray-100">Logos</button>
+                </li>
+                </ul>
+            </div>
+            <div class="relative w-full">
+                <input type="search" id="search-dropdown" class="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500" placeholder="Search Mockups, Logos, Design Templates..." required />
+                <button type="submit" class="absolute top-0 end-0 p-2.5 text-sm font-medium h-full text-white bg-blue-700 rounded-e-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300">
+                    <svg class="w-4 h-4" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/>
+                    </svg>
+                    <span class="sr-only">Search</span>
+                </button>
+            </div>
+        </div>
+    </form>
+</div>
+
+        <!-- Cards For Clothe Items -->
+<div class="flex flex-wrap justify-between mx-2 my-5">
+    <!-- card1 -->
+    <div class="w-full max-w-[48%] bg-white border border-black-200 rounded-lg shadow-sm">
+        <a href="#">
+            <img class="p-8 rounded-t-lg" src="https://images.samsung.com/is/image/samsung/p6pim/ph/2407/gallery/ph-galaxy-watch-ultra-l705-sm-l705fdaaxtc-542169673?$684_547_PNG$" alt="product image" />
+        </a>
+        <div class="px-5 pb-5">
+            <a href="#">
+                <h5 class="text-xl font-semibold tracking-tight text-gray-900">Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport</h5>
+            </a>
+            <div class="flex items-center justify-between">
+                <!-- <span class="text-3xl font-bold text-gray-900">$599</span> -->
+                <a href="#" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
+            </div>
+        </div>
+    </div>
+    <!-- card 2 -->
+    <div class="w-full max-w-[48%] bg-white border border-black-200 rounded-lg shadow-sm">
+        <a href="#">
+            <img class="p-8 rounded-t-lg" src="https://images.samsung.com/is/image/samsung/p6pim/ph/2407/gallery/ph-galaxy-watch-ultra-l705-sm-l705fdaaxtc-542169673?$684_547_PNG$" alt="product image" />
+        </a>
+        <div class="px-5 pb-5">
+            <a href="#">
+                <h5 class="text-xl font-semibold tracking-tight text-gray-900">Apple Watch Series 7 GPS, Aluminium Case, Starlight Sport</h5>
+            </a>
+            <div class="flex items-center justify-between">
+                <!-- <span class="text-3xl font-bold text-gray-900">$599</span> -->
+                <a href="#" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add to cart</a>
+            </div>
+        </div>
+    </div>
+    
 </div>
     <!-- Scripts -->
     <script>
